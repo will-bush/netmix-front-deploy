ba2ef7d6174bb23f8a3caa5728193156680ad97a
Will Bush <williamdcbush@gmail.com>
2020-01-21 09:32:56 +0000

    Updates

---
+++ precache-manifest.305407febb857f047309240bd382cd1b.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4a88637bf06eff2ee0f68bca321a5ef",
+    "url": "/netmix-front-deploy/index.html"
+  },
+  {
+    "revision": "8f04ed16c159b0551b0d",
+    "url": "/netmix-front-deploy/static/css/2.505fe56e.chunk.css"
+  },
+  {
+    "revision": "c56aeb9b4791f492d1ff",
+    "url": "/netmix-front-deploy/static/css/main.d72792c1.chunk.css"
+  },
+  {
+    "revision": "8f04ed16c159b0551b0d",
+    "url": "/netmix-front-deploy/static/js/2.47b43555.chunk.js"
+  },
+  {
+    "revision": "c56aeb9b4791f492d1ff",
+    "url": "/netmix-front-deploy/static/js/main.0dd07b0e.chunk.js"
+  },
+  {
+    "revision": "3fb1f4b6df15c00930c0",
+    "url": "/netmix-front-deploy/static/js/runtime-main.18a37105.js"
+  },
+  {
+    "revision": "e3b42d2a9b5a32a9d0ef02719bfe6aaa",
+    "url": "/netmix-front-deploy/static/media/netmix.e3b42d2a.png"
+  },
+  {
+    "revision": "663195061640a01c4868073155362bbd",
+    "url": "/netmix-front-deploy/static/media/netmix_drop.66319506.png"
+  },
+  {
+    "revision": "805548f58f93ca2da288742d5655ccf6",
+    "url": "/netmix-front-deploy/static/media/poster.805548f5.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/netmix-front-deploy/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/netmix-front-deploy/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/netmix-front-deploy/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/netmix-front-deploy/static/media/slick.f97e3bbf.svg"
+  }
+]);
